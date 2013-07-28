94914b65ffda449db99afc0c69cb1dcbd21e0365
PrinceMukka <Prince.mukka@gmail.com>
2013-07-28 14:59:18 -0700

    Changes to the layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# returns title on a per-page basis
 module ApplicationHelper
+  def title
+    base_title = "Ruby on Rails Tutorials Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
